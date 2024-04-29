bfb2919d0ac5b32bf1bbb67c78aa9b0be6f80db8
Rahul Singh Patel <rahulsinghpatel198@gmail.com>
2024-04-29 16:39:32 +0530

    routes chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-// import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
+
+import  Routes from "./Routes/RoutesComponents"; // Import Routes and Route
 import './App.css';
 import Nav from "../src/Components/Navbar";
-import Home from "../src/Components/Home";
-import Entry from "../src/Components/Entry";
-import Library from "../src/Components/Library";
-import Details from '../src/Components/Details';
+import RoutesComponents from "./Routes/RoutesComponents";
+import axios from "axios";
 
 
-function App() {
+const App= () =>{
+  const getusers = async () =>{
+    try {
+       // jsonplaceholde website me jake neeche user me jake link copy kro
+       const {data} = await  axios.get("https://jsonplaceholder.typicode.com/users")
+    console.log(data)
+
+    } catch (error) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className='w-screen h-screen  bg-sky-100' >
       <Nav/>
-      <Routes>
-
-        <Route path="/"  element={<Home/>}  />
-        <Route path="/create" element={<Entry/>} />
-        <Route path="/show" element={<Library/>} >
-          <Route path="/show/:id" element={<Details/>} />
-        </Route>
-
-      </Routes>
+    <RoutesComponents/>
+    <hr /><hr />
+    <button className="bg-blue-500 py-2 px-5 my-5" onClick={getusers}>Get Uers</button>
     </div>
   );
 }
